328f07690266794f79ddb2e281d5730f2c6a89d2
Andreas Gunnarsson <andreas.gunnarsson@gmail.com>
2020-12-07 23:00:02 +0100

    - Updated ER diagram.

--- SQL_ELGIGANTO.sql
+++ SQL_ELGIGANTO.sql
@@ -15,16 +15,16 @@ DROP TABLE IF EXISTS Reserved;
 DROP TABLE IF EXISTS Storage;
 DROP TABLE IF EXISTS Product;
 
-CREATE TABLE Category (Id int PRIMARY KEY IDENTITY(1,1), [Name] varchar (50) NOT NULL);
+CREATE TABLE Category (Id int PRIMARY KEY IDENTITY(1,1), [Name] varchar (50) NOT NULL UNIQUE);
 CREATE TABLE Popularity (Id int PRIMARY KEY IDENTITY(1,1), ProductId int NOT NULL, Popularity int NOT NULL DEFAULT 0);
 CREATE TABLE Product (Id int PRIMARY KEY IDENTITY(1,1), CategoryId int NOT NULL, [Name] varchar(50) NOT NULL, Price float(53) NOT NULL);
-CREATE TABLE Costumer (Id int PRIMARY KEY IDENTITY(1,1), [Name] varchar(50) NOT NULL, Mail varchar(50) NOT NULL, [Address] varchar(50) NOT NULL);
+CREATE TABLE Costumer (Id int PRIMARY KEY IDENTITY(1,1), [Name] varchar(50) NOT NULL, Mail varchar(50) NOT NULL UNIQUE, [Address] varchar(50) NOT NULL);
 CREATE TABLE [Order] (Id int PRIMARY KEY IDENTITY(1,1), ProductId int NOT NULL, CostumerId int NOT NULL, Ordernumber int NOT NULL DEFAULT CAST(RAND() * 100 AS int), Amount int NOT NULL);
-CREATE TABLE Cart (Id int PRIMARY KEY IDENTITY(1,1), CostumerId int NOT NULL, ProductId int NOT NULL);
-CREATE TABLE Reserved (Id int PRIMARY KEY IDENTITY(1,1), OrderId int, StorageId int);
-CREATE TABLE Storage (Id int PRIMARY KEY IDENTITY(1,1), ProductId int, ReservedId int, Amount int);
+CREATE TABLE Cart (Id int PRIMARY KEY IDENTITY(1,1), CostumerId int NOT NULL, ProductId int NOT NULL, Amount int NOT NULL DEFAULT 1);
+CREATE TABLE Reserved (Id int PRIMARY KEY IDENTITY(1,1), OrderId int NOT NULL, StorageId int NOT NULL);
+CREATE TABLE Storage (Id int PRIMARY KEY IDENTITY(1,1), ProductId int NOT NULL, Amount int NOT NULL, ReservedId int);
 CREATE TABLE StorageTransaction (Id int PRIMARY KEY IDENTITY(1,1), ProductId int, [Time] datetime DEFAULT GETDATE(), Amount int NOT NULL, ReasonId int NOT NULL);
-CREATE TABLE TransactionReason (Id int PRIMARY KEY IDENTITY(1,1), Reason varchar(50) NOT NULL);
+CREATE TABLE TransactionReason (Id int PRIMARY KEY IDENTITY(1,1), Reason varchar(50) NOT NULL UNIQUE);
 
 ALTER TABLE Product ADD CONSTRAINT FK_Product_Category FOREIGN KEY (CategoryId) REFERENCES Category(Id);
 ALTER TABLE Popularity ADD CONSTRAINT FK_Popularity_Product FOREIGN KEY (ProductId) REFERENCES Product(Id);
@@ -40,11 +40,55 @@ ALTER TABLE StorageTransaction ADD CONSTRAINT FK_StorageTrasaction_Product FOREI
 ALTER TABLE StorageTransaction ADD CONSTRAINT FK_StorageTransaction_Reason FOREIGN KEY (ReasonId) REFERENCES TransactionReason(Id);
 
 -- Lägg till tabell för personal?
-INSERT INTO Category ([Name]) VALUES ('Kategori 1'), ('Kategori 2'), ('Kategori 3');
-INSERT INTO TransactionReason (Reason) VALUES ('Reason 1'), ('Reason 2'), ('Reason 3');
-INSERT INTO Product (CategoryId, [Name], Price) VALUES (1, 'grafikkort', 29.99);
+-- Onödigt att ha "Id" och "ProductId" i Storage-tabellen? Är alltid samma sak?
+-- Trigger ifall man editar Storage eller StorageTransaction som gör att man påverkar båda?
+-- Trigger mellan Cart och Order?
+
+-- Hårdkodade värden:
+INSERT INTO Category ([Name]) VALUES ('GPU'), ('CPU'), ('RAM');
+INSERT INTO TransactionReason (Reason) VALUES ('Delivery'), ('Return'), ('Stock adjustment');
+
+-- Förutbestämd testdata:
+INSERT INTO Product (CategoryId, [Name], Price) VALUES (1, 'Voodoo 2', 399.99);
+INSERT INTO Product (CategoryId, [Name], Price) VALUES (1, 'Radeon', 349.99);
+INSERT INTO Product (CategoryId, [Name], Price) VALUES (2, 'AMD 100 MHz', 299.99);
+INSERT INTO Product (CategoryId, [Name], Price) VALUES (2, 'Intel 300 MHz', 599.99);
+INSERT INTO Product (CategoryId, [Name], Price) VALUES (3, 'Noname 128 MB', 49.99);
+INSERT INTO Product (CategoryId, [Name], Price) VALUES (3, 'Intel 512 MB', 199.99);
 INSERT INTO Costumer ([Name], Mail, [Address]) VALUES ('Boris', 'bor@mail.com', 'Hemgatan 2');
-INSERT INTO [Order] (ProductId, CostumerId, Amount) VALUES (1, 1, 2);
+INSERT INTO Costumer ([Name], Mail, [Address]) VALUES ('Greger', 'greger@mail.com', 'Husvägen 3');
+INSERT INTO Storage (ProductId, Amount) VALUES (1, 10), (2, 5), (3, 50), (4, 55), (5, 70), (6, 105);
+
+
+INSERT INTO Cart (CostumerId, ProductId) VALUES (1, 2);			-- Skapa en kundvagn för en kund med en produkt (grafikkort - Radeon).
+UPDATE Cart SET Amount = 2 WHERE Id = 1;						-- Uppdaterar hur många varor kunden har i kundvagnen.
+INSERT INTO [Order] (ProductId, CostumerId, Amount)				-- Kopierar värdena från Cart till [Order]-tabellen (detta för att ge det ett ordernummer).
+SELECT CostumerId, ProductId, Amount FROM Cart
+WHERE Id = 3;
+DELETE FROM Cart WHERE Id = 2;									-- Tar bort Id:t från Cart-tabellen.
+INSERT INTO Reserved (OrderId, StorageId) VALUES ()
+SELECT * FROM Reserved;
+SELECT * FROM [Order];
+SELECT * FROM Storage;
+-- Reservera artikel i lagret.
+-- Leverera order:
+	-- Ta bort reservation-
+	-- Sänk lagersaldo (med "Amount" från [Order]).
+	-- Skapa lagertransaktion (med "TransactionReason" : "Delivery").
+-- Justera lager:
+	-- Ändra lagersaldo
+	-- Skapa en lagertransaktion (med "TransactionReason" : "Stock adjustment").
+-- Returnera:
+	-- ???
+
+-- Andra frågor
+---------------
+-- Hur hanterar man flera varor?
+	-- Antingen har man flera rader i tabellerna Cart och [Order] som man sedan sätter ihop.
+	-- Alternativ: Man har en till tabell med varorna man valt.
+	-- Fulhack: Förutsätt att man endast hanterar en vara. Detta verkar vara det vi ska göra enligt texten?
+-- Vi måste göra en UPDATE Cart på det sista Id:t som lades till.
+-- Trigger då man drar ner (UPDATE) Cart till 0 varor för att ta bort varan.
 
 SELECT * FROM Category;
 SELECT * FROM Popularity;
